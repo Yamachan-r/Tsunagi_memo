fbaf4216acce5aa54497d62ce22220047ff8a6e8
Yamachan-r <r.chun.d.xxx@gmail.com>
2024-11-09 01:13:26 +0900

    add:個人を特定しやすい情報の暗号化

--- app/models/medical_history.rb
+++ app/models/medical_history.rb
@@ -1,6 +1,11 @@
 class MedicalHistory < ApplicationRecord
   belongs_to :user
 
+  encrypts :disease
+  encrypts :treatment_status
+  encrypts :age_at_diagnosis
+  encrypts :disease_notes
+
   validates :disease, presence: true, length: { maximum: 100 }
   validates :treatment_status, length: { maximum: 200 }
   validates :disease_notes, length: { maximum: 30000 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   has_many :family_groups, through: :user_family_groups
   has_many :medical_histories, dependent: :destroy
 
+  encrypts :name
+  encrypts :birth_date
+  encrypts :gender
+  encrypts :uid
+
   validates :name, presence: true, length: { maximum: 40 }
   
   enum blood_type: { A: 0, B: 1, O: 2, AB: 3 }
