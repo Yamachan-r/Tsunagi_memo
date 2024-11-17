a9a4bb4d6d37813ab96c79f7495b398161512d34
Yamachan-r <r.chun.d.xxx@gmail.com>
2024-11-17 22:22:58 +0900

    add:invite_token（招待トークン）カラム作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_08_161910) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_17_132116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_08_161910) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "invite_token"
+    t.index ["invite_token"], name: "index_family_groups_on_invite_token", unique: true
   end
 
   create_table "medical_histories", force: :cascade do |t|
