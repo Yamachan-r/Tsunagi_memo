6ad5195dd2869c570b83eac42412ebcd9b373ee6
Yamachan-r <r.chun.d.xxx@gmail.com>
2024-11-19 14:57:06 +0900

    add:UIDをハッシュ化した。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,14 @@ class SessionsController < ApplicationController
 
   def create
     user_info = request.env["omniauth.auth"]
-    user = User.find_or_create_by(uid: user_info["uid"]) do |u|
+    hashed_uid = User.hash_uid(user_info["uid"]) # UIDをハッシュ化
+  
+    # ハッシュ化されたUIDでユーザーを検索または作成
+    user = User.find_or_create_by(hashed_uid: hashed_uid) do |u|
       u.name = user_info["info"]["name"]
     end
+
+    # セッションにユーザーIDを保存
     session[:user_id] = user.id
     redirect_to family_groups_path, notice: "ログインしました"
   end
