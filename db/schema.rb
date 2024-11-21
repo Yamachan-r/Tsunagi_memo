404f988cc84a1669e1c027bfe30bb0f0fe6f5bbf
Yamachan-r <r.chun.d.xxx@gmail.com>
2024-11-21 15:57:09 +0900

    change:enum使用のためgenderのカラム型を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_21_063611) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_21_065410) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_21_063611) do
     t.string "name", null: false
     t.string "hashed_uid"
     t.string "birth_date"
-    t.string "gender"
+    t.integer "gender"
     t.integer "blood_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
