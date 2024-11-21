36ba7d7345a89dd26ad7621d04dc95e72d46873e
Yamachan-r <r.chun.d.xxx@gmail.com>
2024-11-21 16:33:53 +0900

    add:基本情報の編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,27 @@
-class UserController < ApplicationController
-  def show
-    本人orグループに属しているか
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+
+  def show; #同じグループに属している人が見ることができるように
   end
   
-  def edit
-    本人のみ
-  end
-  
+  def edit; end
+
   def update
-    本人のみ
+    if @user.update(user_params)
+      redirect_to root_path, notice: "更新しました"
+    else
+      flash.now['danger'] = t('defaults.flash_message.not_updated', item: User.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :gender, :birth_date, :blood_type)
   end
 end
